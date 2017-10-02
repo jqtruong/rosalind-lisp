b6ac8ce5362dabdc8973148cd602ebfee8b2baa6
Jerome Truong <jerometruong@gmail.com>
2017-10-02 15:21:03 -0500

    hamm 100%

--- hamm/hamm.lisp
+++ hamm/hamm.lisp
@@ -2,5 +2,17 @@
 
 (in-package #:hamm)
 
-;;; "hamm" goes here. Hacks and glory await!
+(defvar *sample-dataset-1* "GAGCCTACTAACGGGAT")
+(defvar *sample-dataset-2* "CATCGTAATGACGGCCT")
 
+(defvar *sample-output* 7)
+
+(defun distance (s1 s2)
+  "Given: Two DNA strings `S1' and `S2' of equal length (not exceeding 1 kbp).
+
+Return: The Hamming distance."
+
+  (loop for c1 across s1
+        for c2 across s2
+        count (not (char-equal c1 c2)) into distance
+        finally (return distance)))
