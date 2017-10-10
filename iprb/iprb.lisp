e263d39cb0ac863fb2f454f6fc26a4cbbcf42118
Jerome Truong <jerometruong@gmail.com>
2017-10-10 11:41:59 -0500

    wasted time...
    ...trying to make a combination table

--- iprb/iprb.lisp
+++ iprb/iprb.lisp
@@ -15,35 +15,35 @@ Return: The probability that two randomly selected mating organisms will produce
 an individual possessing a dominant allele (and thus displaying the dominant
 phenotype). Assume that any two organisms can mate."
 
-  (let* ((tot1 (+ k m n))
-         (tot2 (1- tot1)))
+  (let* ((ini (+ k m n))                ; initial population
+         (rem (1- ini)))                ; remaining population
     (+
      ;; 1st: k, 2nd: k; 100%
-     (* (/ k tot1)
-        (/ (1- k) tot2))
+     (* (/ k ini)
+        (/ (1- k) rem))
      ;; 1st: k, 2nd: m; 100%
-     (* (/ k tot1)
-        (/ m tot2))
+     (* (/ k ini)
+        (/ m rem))
      ;; 1st: k, 2nd: n; 100%
-     (* (/ k tot1)
-        (/ n tot2))
+     (* (/ k ini)
+        (/ n rem))
      ;; 1st: m, 2nd: k; 100%
-     (* (/ m tot1)
-        (/ k tot2))
+     (* (/ m ini)
+        (/ k rem))
      ;; 1st: m, 2nd: m; 75%
-     (* (/ m tot1)
-        (/ (1- m) tot2)
+     (* (/ m ini)
+        (/ (1- m) rem)
         .75)
      ;; 1st: m, 2nd: n; 50%
-     (* (/ m tot1)
-        (/ n tot2)
+     (* (/ m ini)
+        (/ n rem)
         .5)
      ;; 1st: n, 2nd: k; 100%
-     (* (/ n tot1)
-        (/ k tot2))
+     (* (/ n ini)
+        (/ k rem))
      ;; 1st: n, 2nd: m; 50%
-     (* (/ m tot1)
-        (/ n tot2)
+     (* (/ m ini)
+        (/ n rem)
         .5))))
 
 ;;; Tests
